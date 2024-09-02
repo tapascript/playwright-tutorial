b0abdb71e9aa589a681be6f90bed3ab2c89d5c06
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-09-02 14:49:27 +0530

    Changed the lambdatest scripts

--- 01-playwright/playwright-lambdatest.js
+++ 01-playwright/playwright-lambdatest.js
@@ -1,51 +1,48 @@
-const { chromium } = require('playwright')
-const { expect } = require('@playwright/test');
-require('dotenv').config();
+const { chromium } = require("playwright");
+const { expect } = require("@playwright/test");
+require("dotenv").config();
 
 (async () => {
   const capabilities = {
-    'browserName': 'Chrome', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
-    'browserVersion': 'latest',
-    'LT:Options': {
-      'platform': 'Windows 10',
-      'build': 'Playwright Lambdatest Build',
-      'name': 'Playwright Lambdatest Test',
-      'user': process.env.LT_USERNAME,
-      'accessKey': process.env.LT_ACCESS_KEY,
-      'network': true,
-      'video': true,
-      'console': true
-    }
-  }
+    browserName: "Chrome", // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+    browserVersion: "latest",
+    "LT:Options": {
+      platform: "Windows 10",
+      build: "Playwright Lambdatest Build",
+      name: "Playwright Lambdatest Test",
+      user: process.env.LT_USERNAME,
+      accessKey: process.env.LT_ACCESS_KEY,
+      network: true,
+      video: true,
+      console: true,
+    },
+  };
 
   const browser = await chromium.connect({
-    wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
-  })
+    wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(
+      JSON.stringify(capabilities)
+    )}`,
+  });
 
-  const page = await browser.newPage()
+  const page = await browser.newPage();
 
-  await page.goto('https://www.creowis.com')
-
+  await page.goto("https://ecommerce-playground.lambdatest.io/");
 
   try {
-    await expect(page).toHaveTitle(/CreoWis/);
-    await page.getByRole('link', { name: 'Let’s Start Working' }).click();
-    await page.getByText('Product/Application').click();
-    await page.getByPlaceholder('Your Name').click();
-    await page.getByPlaceholder('Your Name').fill('Test User');
-    await page.getByPlaceholder('Your Name').press('Tab');
-    await page.getByPlaceholder('Your Email').fill('test@email.com');
-    await page.getByPlaceholder('Your Email').press('Tab');
-    await page.getByPlaceholder('Describe your idea / project').fill('Website Building');
-    //await page.getByRole('button', { name: 'Send Message' }).click();
+    await expect(page).toHaveTitle(/Your Store/);
+    await page.getByPlaceholder("Search For Products").first().click();
+    await page.getByPlaceholder("Search For Products").first().fill("Phone");
+    await page.getByRole("button", { name: "SEARCH" }).click();
+    await page.locator(".product-thumb").nth(2).click();
+    await page.getByRole("button", { name: "BUY NOW" }).click();
+
     // Mark the test as completed or failed
-    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: 'New User Subscribed' } })}`)
+    await page.evaluate((_) => {},
+    `lambdatest_action: ${JSON.stringify({ action: "setTestStatus", arguments: { status: "passed", remark: "Phone products searched" } })}`);
   } catch {
-    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'failed', remark: 'New User Subscription Failed' } })}`)
+    await page.evaluate((_) => {},
+    `lambdatest_action: ${JSON.stringify({ action: "setTestStatus", arguments: { status: "failed", remark: "Phone products search Failed" } })}`);
   }
 
- 
-
-
-  await browser.close()
-})()
+  await browser.close();
+})();
